585aa2a07b7af666c4b3e32626219ddf90e601ac
cor22982 <cor22982@uvg.edu.gt>
2024-06-08 14:55:52 -0600

    feat: Add froms

---
+++ Schema/inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO `froms`(`region_state`, `country`) VALUES ('GT','GT'); /*1*/
+INSERT INTO `froms`(`region_state`, `country`) VALUES ('NY','USA');/*2*/
+INSERT INTO `froms`(`region_state`, `country`) VALUES ('FX','USA');/*3*/
+INSERT INTO `froms`(`region_state`, `country`) VALUES ('AL','USA');/*4*/
+INSERT INTO `froms`(`region_state`, `country`) VALUES ('PA','USA');/*5*/
+INSERT INTO `froms`(`region_state`, `country`) VALUES ('CA','USA');/*6*/
+INSERT INTO `froms`(`region_state`, `country`) VALUES ('CT','USA');/*7*/
+
