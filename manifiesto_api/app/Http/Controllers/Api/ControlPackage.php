6a02b380b3f0c34eda5405205ba6f8e1a6def7e2
cor22982 <cor22982@uvg.edu.gt>
2024-06-14 01:54:56 -0600

    feaT: Add constraints to register

--- manifiesto_api/app/Http/Controllers/Api/ControlPackage.php
+++ manifiesto_api/app/Http/Controllers/Api/ControlPackage.php
@@ -10,61 +10,88 @@ use App\Models\From;
 use App\Models\Manifest;
 use App\Models\Package;
 use Illuminate\Support\Facades\Http;
-
+use Illuminate\Support\Facades\Validator;
 
 class ControlPackage extends Controller
 {
     public function store(Request $request)
     {
-        $package = new Package();
-        $package->hawb = $request->hawb;
-        $package->manifest = $request->manifest;
-        $package->weight_lb = $request->weight_lb;
-        $package->weight_kg = 0.0;
-        $package->description_spanish = $request->description_spanish;
-
-        $response = Http::post('http://127.0.0.1:3000/traductor', [
-            'text' => $request->description_spanish,
-            'from_code' => 'es',
-            'to_code' => 'en',
+        $validator = Validator::make($request->all(), [
+            'hawb' => 'required|unique:packages,hawb',
+            'tel_consg' => 'required|string|size:10|unique:clients,telephone',
+            'consing' => 'required|unique:packages,consing',
+        ], [
+            'hawb.unique' => 'El número de hawb ya está registrado.',
+            'tel_consg.required' => 'El número de teléfono es obligatorio.',
+            'tel_consg.string' => 'El número de teléfono debe ser una cadena de texto.',
+            'tel_consg.size' => 'El número de teléfono debe tener exactamente 10 caracteres.',
+            'tel_consg.unique' => 'El número de teléfono ya está registrado.',
+            'consing.unique' => 'El consignatario ya está registrado.',
         ]);
 
-        if ($response->successful()) {
-            // Accede al texto traducido en la respuesta JSON
-            $responseData = $response->json();
-            $package->description_english = $responseData['translated_text'];
-        } else {
-            $package->description_english = 'not found translate';
+        //Validar errores
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
         }
 
-        //shipper
-        $shipper = new Clients();
-        $shipper->name = $request->shipper;
-        $shipper->type = 'SHIPPER';
-        $shipper->telephone = $request->tel_ship;
-        $shipper->id_address = $request->id_ship;
-        $shipper->save();
-        $package->shipper = $request->shipper;
-        //consing
-        $consing = new Clients();
-        $consing->name = $request->consing;
-        $consing->type = 'CONSIGN';
-        $consing->telephone = $request->tel_consg;
-        $consing->id_address = $request->id_consing;
-        $consing->save();
-        $package->consing = $request->consing;
-        //calculos
-        $libras = $request->weight_lb;
-        $valorBulto = 6;
-        while ($libras >= 10) {
-            $valorBulto += 1;
-            $libras -= 10;
+        try {
+            $package = new Package();
+            $package->hawb = $request->hawb;
+            $package->manifest = $request->manifest;
+            $package->weight_lb = $request->weight_lb;
+            $package->weight_kg = 0.0;
+            $package->description_spanish = $request->description_spanish;
+
+            $response = Http::post('http://127.0.0.1:3000/traductor', [
+                'text' => $request->description_spanish,
+                'from_code' => 'es',
+                'to_code' => 'en',
+            ]);
+
+            if ($response->successful()) {
+                // Accede al texto traducido en la respuesta JSON
+                $responseData = $response->json();
+                $package->description_english = $responseData['translated_text'];
+            } else {
+                $package->description_english = 'not found translate';
+            }
+
+            //shipper
+            $shipper = Clients::where('name', $request->shipper)->first();
+            if (!$shipper) {
+                // Crear cliente shipper solo si no existe
+                $shipper = new Clients();
+                $shipper->name = $request->shipper;
+                $shipper->type = 'SHIPPER';
+                $shipper->telephone = $request->tel_ship;
+                $shipper->id_address = $request->id_ship;
+                $shipper->save();
+            }
+            $package->shipper = $shipper->name;
+            //consing
+            $consing = new Clients();
+            $consing->name = $request->consing;
+            $consing->type = 'CONSIGN';
+            $consing->telephone = $request->tel_consg;
+            $consing->id_address = $request->id_consing;
+            $consing->save();
+            $package->consing = $request->consing;
+            //calculos
+            $libras = $request->weight_lb;
+            $valorBulto = 6;
+            while ($libras >= 10) {
+                $valorBulto += 1;
+                $libras -= 10;
+            }
+            $package->custom_value = $valorBulto;
+            $package->type_bag = $request->type_bag;
+            $package->atendend = $request->atendend;
+            $package->bag = $request->bag;
+            $package->save();
+        }catch (\Exception $e) {
+            // Capturar cualquier excepción no esperada
+            return response()->json(['error' => 'Ha ocurrido un error al procesar la solicitud'], 500);
         }
-        $package->custom_value = $valorBulto;
-        $package->type_bag = $request->type_bag;
-        $package->atendend = $request->atendend;
-        $package->bag = $request->bag;
-        $package->save();
     }
 
     public function store_manifest(Request $request){
