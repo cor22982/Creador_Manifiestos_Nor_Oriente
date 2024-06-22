c10fa22fec37354fa23e2cf7607464e783a3183d
cor22982 <cor22982@uvg.edu.gt>
2024-06-22 14:29:05 -0600

    feat: Complete popUp

--- fronted/vite.config.js
+++ fronted/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 5000
+  },
+  resolve: {
+    alias: {
+      '@components': '/src/Components',
+    }
+  },
+  base: '', 
 })
