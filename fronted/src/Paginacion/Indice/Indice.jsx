a0544690bb5b2e99a5830bb423459604e9915a83
cor22982 <cor22982@uvg.edu.gt>
2024-06-23 16:15:47 -0600

    feat: Complete navigation

--- fronted/src/Paginacion/Indice/Indice.jsx
+++ fronted/src/Paginacion/Indice/Indice.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
 import './Indice.css'
+import MenuButton from '@components/MenuButton'
+import { faHome, faFile } from '@fortawesome/free-solid-svg-icons';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 function Indice() {
   return (
     <div className='pantalla-indice'>
-      <div className="opciones-navegacion"></div>
+       <Router>
+      <div className="opciones-navegacion">
+        <MenuButton icono={faHome} goto='/'></MenuButton>
+        <MenuButton icono={faFile} goto='/reports'></MenuButton>
+        <MenuButton  text= 'GT' goto='/guatemala'></MenuButton>
+        <MenuButton  text= 'USA' goto='/estados_unidos'></MenuButton>
+      </div>
+      <div className="interfaz">
+      <Routes>
+              <Route path="/estados_unidos" element={<About />} />
+              <Route path="/guatemala" element={<Users />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/reports" element={<Report/>}></Route>
+            </Routes>
+      </div>
+      </Router>
     </div>
   )
 }
 
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function Report() {
+  return <h2>Report</h2>;
+}
 export default Indice
