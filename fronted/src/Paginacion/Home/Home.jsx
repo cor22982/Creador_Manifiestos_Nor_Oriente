1d3eed3dd9547599c22f29d706709660888c164f
cor22982 <cor22982@uvg.edu.gt>
2024-06-25 02:14:20 -0600

    feat: Complete header information

--- fronted/src/Paginacion/Home/Home.jsx
+++ fronted/src/Paginacion/Home/Home.jsx
@@ -1,17 +1,43 @@
 import React from 'react'
 import './Home.css'
-import useApi from '@hooks/useApi';
 import useCode from '@hooks/useCode'
+import LabelCustom from '@components/LabelCustom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+
 function Home() {
   const { code } = useCode();
   return (
     <div className="total">
       <div className="titulos" >
         <h2 className='titulo-pagina'>CREACION DE BULTOS</h2>
-        <h2 className='titulo-pagina'>{ '202 - ' + code}</h2>
+        <h2 className='titulo-pagina' style={{fontSize: '20px'}}>{ '202 - ' + code}</h2>
       </div>
       <div className="linea"></div>
-      <div className="informacion"></div>
+      <br></br>
+      <div className="informacion">
+        <LabelCustom 
+          titule= '35'
+          simbol= 'No'></LabelCustom>
+        <LabelCustom 
+          titule= '1000'
+          simbol= 'KG'></LabelCustom>
+        <LabelCustom 
+          titule= '35'
+          simbol= 'Frios'></LabelCustom>
+        <LabelCustom 
+          titule= '35'
+          simbol= 'Secos'></LabelCustom>
+        <LabelCustom 
+          titule= '2204.62'
+          simbol= 'LB'></LabelCustom>
+        <br></br>
+        <button className='icono-boton-home'>
+          <FontAwesomeIcon
+            icon={faDownload}
+            ></FontAwesomeIcon>
+        </button>
+      </div>
     </div>
   )
 }
