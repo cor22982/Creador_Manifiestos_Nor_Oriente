9610f708347df3a8e07859ca7a62f3bf28cf94c2
cor22982 <cor22982@uvg.edu.gt>
2024-07-06 22:57:22 -0600

    feat: Register

---
+++ fronted/src/Components/InputForm/InputForm.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './InputForm.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function InputForm({ iconin, width_input,titule, height_input, font }) {
+  const [value, setValue] = useState('');
+
+  return (
+    <div>
+      <div className='main-input'>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder=" "
+          style={{width: width_input, height: height_input,fontSize: font}}
+        />
+        <div className='icon-form'>
+          <FontAwesomeIcon icon={iconin} />
+        </div>
+        <span className={value.length === 0 ? '' : 'fill'}>{titule}</span>
+      </div>
+    </div>
+  );
+}
+
+InputForm.defaultProps = {
+  height_input: '30px' 
+};
+export default InputForm;
